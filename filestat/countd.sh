004b62fd0a01fd788291d3b3cc0fefd1971fa270
Sergey Linev <S.Linev@gsi.de>
2020-03-11 17:00:46 +0100

    Add filestatistic scripts

---
+++ filestat/countd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find . -name '*.d' -type f -printf '%b.%f\0' |
+
+  awk -F . -v RS='\0' '
+
+    {s[$NF] += $1; n[$NF]++}
+
+    END {for (e in s) printf "%15d %4d %s\n", s[e]*512, n[e], e}' |
+
+  sort -n
---
+++ filestat/readme.md
@@ -0,0 +1,11 @@
+#Count file statistic
+
+All files extenstion
+
+    ./count.sh
+
+Only "d" files extenstion
+
+    ./countd.sh
+
+Found: https://unix.stackexchange.com/questions/308846
