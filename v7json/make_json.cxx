c500b1f8e4e023747a11c83ae46688c7fe30b6c1
Sergey Linev <S.Linev@gsi.de>
2021-05-05 09:29:55 +0200

    add more v7 tutorials

--- v7json/make_json.cxx
+++ v7json/make_json.cxx
@@ -22,6 +22,20 @@ void make_json()
    // like root --web=batch make_json.cxx
 
    do_macro("draw_axes.cxx", "axes.json");
-   do_macro("draw_rh1.cxx", "draw_rh1.json");
-   do_macro("draw_rh2.cxx", "draw_rh2.json");
+   do_macro("draw_frame.cxx", "frame.json");
+   do_macro("draw_legend.cxx", "legend.json");
+   do_macro("draw_pave.cxx", "pave.json");
+   do_macro("draw_rh1.cxx", "rh1.json");
+   do_macro("draw_rh2.cxx", "rh2.json");
+   do_macro("draw_rh2_colz.cxx", "rh2_colz.json");
+   do_macro("draw_rh3.cxx", "rh3.json");
+   do_macro("draw_subpads.cxx", "subpads.json");
+   do_macro("draw_text.cxx", "text.json");
+   do_macro("draw_v6.cxx", "v6.json");
+   do_macro("line.cxx", "line.json");
+   do_macro("lineRStyle.cxx", "lineRStyle.json");
+   do_macro("lineStyle.cxx", "lineStyle.json");
+   do_macro("lineWidth.cxx", "lineWidth.json");
+   do_macro("markerStyle.cxx", "markerStyle.json");
+   do_macro("pad.cxx", "pad.json");
 }
