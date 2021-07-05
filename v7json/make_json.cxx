a0b7fade95e4eaf77eb7e36437f8bea14456de5f
Sergey Linev <S.Linev@gsi.de>
2021-07-05 13:21:24 +0200

    New RCanvas tutorials

--- v7json/make_json.cxx
+++ v7json/make_json.cxx
@@ -8,7 +8,7 @@ void do_macro(const char *macroname, const char *jsonname)
 
    printf("Producing %s ...\n", jsonname);
 
-   gInterpreter->ExecuteMacro(Form("$HOME/git/webgui/tutorials/v7/%s",macroname));
+   gInterpreter->ExecuteMacro(Form("$HOME/git/webgui/tutorials/rcanvas/%s",macroname));
 
    auto canv = RCanvas::GetCanvases()[0];
 
@@ -27,24 +27,27 @@ void make_json(const char *sel = nullptr)
 
    if (sel) selectJson = sel;
 
-   do_macro("box.cxx", "box.json");
-   do_macro("draw_axes.cxx", "axes.json");
-   do_macro("draw_frame.cxx", "frame.json");
-   do_macro("draw_legend.cxx", "legend.json");
-   do_macro("draw_pave.cxx", "pave.json");
-   do_macro("draw_rh1.cxx", "rh1.json");
-   do_macro("draw_rh1_twoaxes.cxx", "rh1_twoaxes.json");
-   do_macro("draw_rh2.cxx", "rh2.json");
-   do_macro("draw_rh2_colz.cxx", "rh2_colz.json");
-   do_macro("draw_rh3.cxx", "rh3.json");
-   do_macro("draw_subpads.cxx", "subpads.json");
-   do_macro("draw_symlog.cxx", "symlog.json");
-   do_macro("draw_text.cxx", "text.json");
-   do_macro("draw_v6.cxx", "v6.json");
-   do_macro("line.cxx", "line.json");
-   do_macro("lineRStyle.cxx", "lineRStyle.json");
-   do_macro("lineStyle.cxx", "lineStyle.json");
-   do_macro("lineWidth.cxx", "lineWidth.json");
-   do_macro("markerStyle.cxx", "markerStyle.json");
-   do_macro("pad.cxx", "pad.json");
+   do_macro("raxis.cxx", "raxis.json");
+   do_macro("rbox.cxx", "rbox.json");
+   do_macro("rframe.cxx", "rframe.json");
+   do_macro("rh1_large.cxx", "rh1_large.json");
+   do_macro("rh1_twoaxes.cxx", "rh1_twoaxes.json");
+   do_macro("rh1.cxx", "rh1.json");
+   do_macro("rh2_colz.cxx", "rh2_colz.json");
+   do_macro("rh2_large.cxx", "rh2_large.json");
+   do_macro("rh2.cxx", "rh2.json");
+   do_macro("rh3_large.cxx", "rh3_large.json");
+   do_macro("rh3.cxx", "rh3.json");
+   do_macro("rlegend.cxx", "rlegend.json");
+   do_macro("rline_style.cxx", "rline_style.json");
+   do_macro("rline_width.cxx", "rline_width.json");
+   do_macro("rline.cxx", "rline.json");
+   do_macro("rmarker.cxx", "rmarker.json");
+   do_macro("rpad.cxx", "rpad.json");
+   do_macro("rpave.cxx", "rpave.json");
+   do_macro("rstyle.cxx", "rstyle.json");
+   do_macro("rtext.cxx", "rtext.json");
+   do_macro("subpads.cxx", "subpads.json");
+   do_macro("symlog.cxx", "symlog.json");
+   do_macro("tobject.cxx", "tobject.json");
 }
