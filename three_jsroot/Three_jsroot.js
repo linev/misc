883d0dee0c3e8d51bc1d18d566431212983d6b40
Sergey Linev <S.Linev@gsi.de>
2021-11-26 18:04:39 +0100

    try to create three package for jsroot

---
+++ three_jsroot/Three_jsroot.js
@@ -0,0 +1,202 @@
+import { REVISION } from './node_modules/three/src/constants.js';
+
+export { WebGLMultipleRenderTargets } from './node_modules/three/src/renderers/WebGLMultipleRenderTargets.js';
+export { WebGLMultisampleRenderTarget } from './node_modules/three/src/renderers/WebGLMultisampleRenderTarget.js';
+export { WebGLCubeRenderTarget } from './node_modules/three/src/renderers/WebGLCubeRenderTarget.js';
+export { WebGLRenderTarget } from './node_modules/three/src/renderers/WebGLRenderTarget.js';
+export { WebGLRenderer } from './node_modules/three/src/renderers/WebGLRenderer.js';
+export { WebGL1Renderer } from './node_modules/three/src/renderers/WebGL1Renderer.js';
+export { ShaderLib } from './node_modules/three/src/renderers/shaders/ShaderLib.js';
+export { UniformsLib } from './node_modules/three/src/renderers/shaders/UniformsLib.js';
+export { UniformsUtils } from './node_modules/three/src/renderers/shaders/UniformsUtils.js';
+export { ShaderChunk } from './node_modules/three/src/renderers/shaders/ShaderChunk.js';
+export { FogExp2 } from './node_modules/three/src/scenes/FogExp2.js';
+export { Fog } from './node_modules/three/src/scenes/Fog.js';
+export { Scene } from './node_modules/three/src/scenes/Scene.js';
+export { Sprite } from './node_modules/three/src/objects/Sprite.js';
+export { LOD } from './node_modules/three/src/objects/LOD.js';
+export { SkinnedMesh } from './node_modules/three/src/objects/SkinnedMesh.js';
+export { Skeleton } from './node_modules/three/src/objects/Skeleton.js';
+export { Bone } from './node_modules/three/src/objects/Bone.js';
+export { Mesh } from './node_modules/three/src/objects/Mesh.js';
+export { InstancedMesh } from './node_modules/three/src/objects/InstancedMesh.js';
+export { LineSegments } from './node_modules/three/src/objects/LineSegments.js';
+export { LineLoop } from './node_modules/three/src/objects/LineLoop.js';
+export { Line } from './node_modules/three/src/objects/Line.js';
+export { Points } from './node_modules/three/src/objects/Points.js';
+export { Group } from './node_modules/three/src/objects/Group.js';
+export { VideoTexture } from './node_modules/three/src/textures/VideoTexture.js';
+export { DataTexture } from './node_modules/three/src/textures/DataTexture.js';
+export { DataTexture2DArray } from './node_modules/three/src/textures/DataTexture2DArray.js';
+export { DataTexture3D } from './node_modules/three/src/textures/DataTexture3D.js';
+export { CompressedTexture } from './node_modules/three/src/textures/CompressedTexture.js';
+export { CubeTexture } from './node_modules/three/src/textures/CubeTexture.js';
+export { CanvasTexture } from './node_modules/three/src/textures/CanvasTexture.js';
+export { DepthTexture } from './node_modules/three/src/textures/DepthTexture.js';
+export { Texture } from './node_modules/three/src/textures/Texture.js';
+export * from './node_modules/three/src/geometries/Geometries.js';
+export * from './node_modules/three/src/materials/Materials.js';
+export { AnimationLoader } from './node_modules/three/src/loaders/AnimationLoader.js';
+export { CompressedTextureLoader } from './node_modules/three/src/loaders/CompressedTextureLoader.js';
+export { CubeTextureLoader } from './node_modules/three/src/loaders/CubeTextureLoader.js';
+export { DataTextureLoader } from './node_modules/three/src/loaders/DataTextureLoader.js';
+export { TextureLoader } from './node_modules/three/src/loaders/TextureLoader.js';
+export { ObjectLoader } from './node_modules/three/src/loaders/ObjectLoader.js';
+export { MaterialLoader } from './node_modules/three/src/loaders/MaterialLoader.js';
+export { BufferGeometryLoader } from './node_modules/three/src/loaders/BufferGeometryLoader.js';
+export { DefaultLoadingManager, LoadingManager } from './node_modules/three/src/loaders/LoadingManager.js';
+export { ImageLoader } from './node_modules/three/src/loaders/ImageLoader.js';
+export { ImageBitmapLoader } from './node_modules/three/src/loaders/ImageBitmapLoader.js';
+export { FileLoader } from './node_modules/three/src/loaders/FileLoader.js';
+export { Loader } from './node_modules/three/src/loaders/Loader.js';
+export { LoaderUtils } from './node_modules/three/src/loaders/LoaderUtils.js';
+export { Cache } from './node_modules/three/src/loaders/Cache.js';
+export { AudioLoader } from './node_modules/three/src/loaders/AudioLoader.js';
+export { SpotLight } from './node_modules/three/src/lights/SpotLight.js';
+export { PointLight } from './node_modules/three/src/lights/PointLight.js';
+export { RectAreaLight } from './node_modules/three/src/lights/RectAreaLight.js';
+export { HemisphereLight } from './node_modules/three/src/lights/HemisphereLight.js';
+export { HemisphereLightProbe } from './node_modules/three/src/lights/HemisphereLightProbe.js';
+export { DirectionalLight } from './node_modules/three/src/lights/DirectionalLight.js';
+export { AmbientLight } from './node_modules/three/src/lights/AmbientLight.js';
+export { AmbientLightProbe } from './node_modules/three/src/lights/AmbientLightProbe.js';
+export { Light } from './node_modules/three/src/lights/Light.js';
+export { LightProbe } from './node_modules/three/src/lights/LightProbe.js';
+export { StereoCamera } from './node_modules/three/src/cameras/StereoCamera.js';
+export { PerspectiveCamera } from './node_modules/three/src/cameras/PerspectiveCamera.js';
+export { OrthographicCamera } from './node_modules/three/src/cameras/OrthographicCamera.js';
+export { CubeCamera } from './node_modules/three/src/cameras/CubeCamera.js';
+export { ArrayCamera } from './node_modules/three/src/cameras/ArrayCamera.js';
+export { Camera } from './node_modules/three/src/cameras/Camera.js';
+export { AudioListener } from './node_modules/three/src/audio/AudioListener.js';
+export { PositionalAudio } from './node_modules/three/src/audio/PositionalAudio.js';
+export { AudioContext } from './node_modules/three/src/audio/AudioContext.js';
+export { AudioAnalyser } from './node_modules/three/src/audio/AudioAnalyser.js';
+export { Audio } from './node_modules/three/src/audio/Audio.js';
+export { VectorKeyframeTrack } from './node_modules/three/src/animation/tracks/VectorKeyframeTrack.js';
+export { StringKeyframeTrack } from './node_modules/three/src/animation/tracks/StringKeyframeTrack.js';
+export { QuaternionKeyframeTrack } from './node_modules/three/src/animation/tracks/QuaternionKeyframeTrack.js';
+export { NumberKeyframeTrack } from './node_modules/three/src/animation/tracks/NumberKeyframeTrack.js';
+export { ColorKeyframeTrack } from './node_modules/three/src/animation/tracks/ColorKeyframeTrack.js';
+export { BooleanKeyframeTrack } from './node_modules/three/src/animation/tracks/BooleanKeyframeTrack.js';
+export { PropertyMixer } from './node_modules/three/src/animation/PropertyMixer.js';
+export { PropertyBinding } from './node_modules/three/src/animation/PropertyBinding.js';
+export { KeyframeTrack } from './node_modules/three/src/animation/KeyframeTrack.js';
+export { AnimationUtils } from './node_modules/three/src/animation/AnimationUtils.js';
+export { AnimationObjectGroup } from './node_modules/three/src/animation/AnimationObjectGroup.js';
+export { AnimationMixer } from './node_modules/three/src/animation/AnimationMixer.js';
+export { AnimationClip } from './node_modules/three/src/animation/AnimationClip.js';
+export { Uniform } from './node_modules/three/src/core/Uniform.js';
+export { InstancedBufferGeometry } from './node_modules/three/src/core/InstancedBufferGeometry.js';
+export { BufferGeometry } from './node_modules/three/src/core/BufferGeometry.js';
+export { InterleavedBufferAttribute } from './node_modules/three/src/core/InterleavedBufferAttribute.js';
+export { InstancedInterleavedBuffer } from './node_modules/three/src/core/InstancedInterleavedBuffer.js';
+export { InterleavedBuffer } from './node_modules/three/src/core/InterleavedBuffer.js';
+export { InstancedBufferAttribute } from './node_modules/three/src/core/InstancedBufferAttribute.js';
+export { GLBufferAttribute } from './node_modules/three/src/core/GLBufferAttribute.js';
+export * from './node_modules/three/src/core/BufferAttribute.js';
+export { Object3D } from './node_modules/three/src/core/Object3D.js';
+export { Raycaster } from './node_modules/three/src/core/Raycaster.js';
+export { Layers } from './node_modules/three/src/core/Layers.js';
+export { EventDispatcher } from './node_modules/three/src/core/EventDispatcher.js';
+export { Clock } from './node_modules/three/src/core/Clock.js';
+export { QuaternionLinearInterpolant } from './node_modules/three/src/math/interpolants/QuaternionLinearInterpolant.js';
+export { LinearInterpolant } from './node_modules/three/src/math/interpolants/LinearInterpolant.js';
+export { DiscreteInterpolant } from './node_modules/three/src/math/interpolants/DiscreteInterpolant.js';
+export { CubicInterpolant } from './node_modules/three/src/math/interpolants/CubicInterpolant.js';
+export { Interpolant } from './node_modules/three/src/math/Interpolant.js';
+export { Triangle } from './node_modules/three/src/math/Triangle.js';
+export * as MathUtils from './node_modules/three/src/math/MathUtils.js';
+export { Spherical } from './node_modules/three/src/math/Spherical.js';
+export { Cylindrical } from './node_modules/three/src/math/Cylindrical.js';
+export { Plane } from './node_modules/three/src/math/Plane.js';
+export { Frustum } from './node_modules/three/src/math/Frustum.js';
+export { Sphere } from './node_modules/three/src/math/Sphere.js';
+export { Ray } from './node_modules/three/src/math/Ray.js';
+export { Matrix4 } from './node_modules/three/src/math/Matrix4.js';
+export { Matrix3 } from './node_modules/three/src/math/Matrix3.js';
+export { Box3 } from './node_modules/three/src/math/Box3.js';
+export { Box2 } from './node_modules/three/src/math/Box2.js';
+export { Line3 } from './node_modules/three/src/math/Line3.js';
+export { Euler } from './node_modules/three/src/math/Euler.js';
+export { Vector4 } from './node_modules/three/src/math/Vector4.js';
+export { Vector3 } from './node_modules/three/src/math/Vector3.js';
+export { Vector2 } from './node_modules/three/src/math/Vector2.js';
+export { Quaternion } from './node_modules/three/src/math/Quaternion.js';
+export { Color } from './node_modules/three/src/math/Color.js';
+export { SphericalHarmonics3 } from './node_modules/three/src/math/SphericalHarmonics3.js';
+export { SpotLightHelper } from './node_modules/three/src/helpers/SpotLightHelper.js';
+export { SkeletonHelper } from './node_modules/three/src/helpers/SkeletonHelper.js';
+export { PointLightHelper } from './node_modules/three/src/helpers/PointLightHelper.js';
+export { HemisphereLightHelper } from './node_modules/three/src/helpers/HemisphereLightHelper.js';
+export { GridHelper } from './node_modules/three/src/helpers/GridHelper.js';
+export { PolarGridHelper } from './node_modules/three/src/helpers/PolarGridHelper.js';
+export { DirectionalLightHelper } from './node_modules/three/src/helpers/DirectionalLightHelper.js';
+export { CameraHelper } from './node_modules/three/src/helpers/CameraHelper.js';
+export { BoxHelper } from './node_modules/three/src/helpers/BoxHelper.js';
+export { Box3Helper } from './node_modules/three/src/helpers/Box3Helper.js';
+export { PlaneHelper } from './node_modules/three/src/helpers/PlaneHelper.js';
+export { ArrowHelper } from './node_modules/three/src/helpers/ArrowHelper.js';
+export { AxesHelper } from './node_modules/three/src/helpers/AxesHelper.js';
+export * from './node_modules/three/src/extras/curves/Curves.js';
+export { Shape } from './node_modules/three/src/extras/core/Shape.js';
+export { Path } from './node_modules/three/src/extras/core/Path.js';
+export { ShapePath } from './node_modules/three/src/extras/core/ShapePath.js';
+export { CurvePath } from './node_modules/three/src/extras/core/CurvePath.js';
+export { Curve } from './node_modules/three/src/extras/core/Curve.js';
+export { DataUtils } from './node_modules/three/src/extras/DataUtils.js';
+export { ImageUtils } from './node_modules/three/src/extras/ImageUtils.js';
+export { ShapeUtils } from './node_modules/three/src/extras/ShapeUtils.js';
+export { PMREMGenerator } from './node_modules/three/src/extras/PMREMGenerator.js';
+export { WebGLUtils } from './node_modules/three/src/renderers/webgl/WebGLUtils.js';
+export * from './node_modules/three/src/constants.js';
+export * from './node_modules/three/src/Three.Legacy.js';
+
+// jsroot part
+export { TextGeometry } from './node_modules/three/examples/jsm/geometries/TextGeometry.js';
+
+import * as json1 from "./node_modules/three/examples/fonts/helvetiker_regular.typeface.json";
+
+let HelveticerRegularFont = new Font(json1);
+
+export { HelveticerRegularFont };
+
+export { OrbitControls} from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
+export { TransformControls } from "./node_modules/three/examples/jsm/controls/TransformControls.js";
+export { CopyShader } from "./node_modules/three/examples/jsm/shaders/CopyShader.js";
+export { EffectComposer } from "./node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+export { MaskPass } from "./node_modules/three/examples/jsm/postprocessing/MaskPass.js";
+export { RenderPass } from "./node_modules/three/examples/jsm/postprocessing/RenderPass.js";
+export { ShaderPass } from "./node_modules/three/examples/jsm/postprocessing/ShaderPass.js";
+export { SSAOShader } from "./node_modules/three/examples/jsm/shaders/SSAOShader.js";
+export { SSAOPass } from "./node_modules/three/examples/jsm/postprocessing/SSAOPass.js";
+export { SimplexNoise } from "./node_modules/three/examples/jsm/math/SimplexNoise.js";
+export { LuminosityHighPassShader } from "./node_modules/three/examples/jsm/shaders/LuminosityHighPassShader.js";
+export { UnrealBloomPass } from "./node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
+// end of jsroot part
+
+
+if ( typeof __THREE_DEVTOOLS__ !== 'undefined' ) {
+
+	/* eslint-disable no-undef */
+	__THREE_DEVTOOLS__.dispatchEvent( new CustomEvent( 'register', { detail: {
+		revision: REVISION,
+	} } ) );
+	/* eslint-enable no-undef */
+
+}
+
+if ( typeof window !== 'undefined' ) {
+
+	if ( window.__THREE__ ) {
+
+		console.warn( 'WARNING: Multiple instances of Three.js being imported.' );
+
+	} else {
+
+		window.__THREE__ = REVISION;
+
+	}
+
+}
