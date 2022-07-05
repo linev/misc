4a38195dbd9eb00b64eb70d325d7e1f822721cc7
Sergey Linev <S.Linev@gsi.de>
2022-07-05 10:35:54 +0200

    Add Accept-Ranges to response header

--- server/server.py
+++ server/server.py
@@ -101,6 +101,7 @@ class RangeRequestHandler(SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
     def send_my_headers(self):
+        self.send_header('Accept-Ranges', 'bytes')
         self.send_header("Access-Control-Allow-Origin", "*")
         if self.path.count(".json.gz") == 1 and (self.path.count("?") == 0 or self.path.count("?compact") == 1 or self.path.count("?method") == 1):
            self.send_header("Content-Encoding", "gzip")
