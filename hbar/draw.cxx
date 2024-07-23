3d206ad38cb5acc7512f860f05869d4c45b787c6
Sergey Linev <S.Linev@gsi.de>
2024-07-23 19:10:12 +0200

    First three datasheets

--- hbar/draw.cxx
+++ hbar/draw.cxx
@@ -83,7 +83,7 @@ std::string remap_title(const std::string &src)
    return src.substr(0, 40);
 }
 
-void draw(const std::string &fname = "11-6-8-2.xlsx")
+void draw(const std::string &fname = "11-6-8-3.xlsx")
 {
    std::string csv_name;
 
